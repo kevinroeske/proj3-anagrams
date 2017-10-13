35c2bbc50ac8166576d12cc70cc8171266f66408
Kevin Roeske <kroeske@localhost.localdomain>
2017-10-13 14:15:44 -0700

    working solution

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -74,7 +74,7 @@ def success():
 #######################
 
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check")
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -85,9 +85,9 @@ def check():
     already found.
     """
     app.logger.debug("Entering check")
-
+    rslt = ""
     # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
+    text = flask.request.args.get("text", type=str)
     jumble = flask.session["jumble"]
     matches = flask.session.get("matches", [])  # Default to empty list
 
@@ -100,22 +100,30 @@ def check():
         # Cool, they found a new word
         matches.append(text)
         flask.session["matches"] = matches
-    elif text in matches:
-        flask.flash("You already found {}".format(text))
-    elif not matched:
-        flask.flash("{} isn't in the list of words".format(text))
-    elif not in_jumble:
-        flask.flash(
-            '"{}" can\'t be made from the letters {}'.format(text, jumble))
-    else:
-        app.logger.debug("This case shouldn't happen!")
-        assert False  # Raises AssertionError
+        app.logger.info("Found a word")
+        rslt = {"word_found": True, "is_done": False};
+#    elif text in matches:
+#        flask.flash("You already found {}".format(text))
+#    elif not matched:
+#        flask.flash("{} isn't in the list of words".format(text))
+#    elif not in_jumble:
+#        flask.flash(
+#            '"{}" can\'t be made from the letters {}'.format(text, jumble))
+#    else:
+#        app.logger.debug("This case shouldn't happen!")
+#        assert False  # Raises AssertionError
 
     # Choose page:  Solved enough, or keep going?
     if len(matches) >= flask.session["target_count"]:
-       return flask.redirect(flask.url_for("success"))
+        app.logger.info("DONE!")
+        rslt["is_done"] = True
+        return flask.jsonify(result=rslt)
     else:
-       return flask.redirect(flask.url_for("keep_going"))
+        app.logger.info("Keep going")
+        return flask.jsonify(result=rslt)
+
+
+
 
 ###############
 # AJAX request handlers
